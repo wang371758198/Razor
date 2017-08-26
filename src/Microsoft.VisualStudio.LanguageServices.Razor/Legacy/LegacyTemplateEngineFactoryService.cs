9c08cb46e565678113ce362a855a2fb55c58f2ea
Ryan Nowak <nowakra@gmail.com>
2017-08-25 18:07:07 -0700

    WIP

--- src/Microsoft.VisualStudio.LanguageServices.Razor/Legacy/LegacyTemplateEngineFactoryService.cs
+++ src/Microsoft.VisualStudio.LanguageServices.Razor/Legacy/LegacyTemplateEngineFactoryService.cs
@@ -3,8 +3,11 @@
 
 using System;
 using System.ComponentModel.Composition;
+using System.IO;
 using Microsoft.AspNetCore.Mvc.Razor.Extensions;
 using Microsoft.AspNetCore.Razor.Language;
+using Microsoft.VisualStudio.Shell;
+using Microsoft.VisualStudio.Shell.Interop;
 
 namespace Microsoft.VisualStudio.LanguageServices.Razor
 {
@@ -15,12 +18,52 @@ namespace Microsoft.VisualStudio.LanguageServices.Razor
     [Export(typeof(RazorTemplateEngineFactoryService))]
     internal class LegacyTemplateEngineFactoryService : RazorTemplateEngineFactoryService
     {
+        private readonly IServiceProvider _services;
+        
+        [ImportingConstructor]
+        public LegacyTemplateEngineFactoryService([Import(typeof(SVsServiceProvider))] IServiceProvider services)
+        {
+            if (services == null)
+            {
+                throw new ArgumentNullException(nameof(services));
+            }
+
+            _services = services;
+        }
+        
         public override RazorTemplateEngine Create(string projectPath, Action<IRazorEngineBuilder> configure)
         {
             if (projectPath == null)
             {
                 throw new ArgumentNullException(nameof(projectPath));
             }
+
+            var project = GetProject(projectPath);
+            if (project == null)
+            {
+
+            }
+
+            var hierarchy = (IVsHierarchy)project;
+            ErrorHandler.ThrowOnFailure(hierarchy.GetProperty((uint)VSConstants.VSITEMID.Root, (int)__VSHPROPID.VSHPROPID_ExtObject, out var obj));
+
+            var dteProject = (EnvDTE.Project)obj;
+            var vsProject = (VSLangProj.VSProject)dteProject.Object;
+
+            var count = vsProject.References.Count;
+            foreach (VSLangProj.Reference reference in vsProject.References)
+            {
+                if (reference.Name == "Microsoft.AspNetCore.Mvc.Razor")
+                {
+                    Console.WriteLine(reference);
+                }
+                else if (reference.Name == "Microsoft.AspNetCore.Razor.Language")
+                {
+                    Console.WriteLine(reference);
+                }
+            }
+
+
 
             var engine = RazorEngine.CreateDesignTime(b =>
             {
@@ -33,6 +76,35 @@ namespace Microsoft.VisualStudio.LanguageServices.Razor
             var templateEngine = new MvcRazorTemplateEngine(engine, RazorProject.Create(projectPath));
             templateEngine.Options.ImportsFileName = "_ViewImports.cshtml";
             return templateEngine;
+        }
+
+        private IVsProject GetProject(string projectPath)
+        {
+            var solution = (IVsSolution)_services.GetService(typeof(SVsSolution));
+
+            var guid = Guid.Empty;
+            ErrorHandler.ThrowOnFailure(solution.GetProjectEnum((uint)__VSENUMPROJFLAGS.EPF_LOADEDINSOLUTION, ref guid, out var enumerator));
+            ErrorHandler.ThrowOnFailure(enumerator.Reset());
+
+            var hierarchy = new IVsHierarchy[1] { null };
+            while (ErrorHandler.ThrowOnFailure(enumerator.Next(1, hierarchy, out var fetched)) == VSConstants.S_OK && fetched == 1)
+            {
+                var project = (IVsProject)hierarchy[0];
+                ErrorHandler.ThrowOnFailure(project.GetMkDocument((uint)VSConstants.VSITEMID.Root, out var path), VSConstants.E_NOTIMPL);
+
+                if (path != null)
+                {
+                    // The path returned by GetMkDocument is the path to the project file. We want the path of the project directory.
+                    path = Path.GetDirectoryName(path);
+                }
+
+                if (string.Equals(path, projectPath, StringComparison.OrdinalIgnoreCase))
+                {
+                    return project;
+                }
+            }
+
+            return null;
         }
     }
 }
